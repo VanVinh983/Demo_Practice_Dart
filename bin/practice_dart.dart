0bd1a4b2a387ed78a437f4963186be3a9491a6ce
EvolViper <74658373+EvolViper@users.noreply.github.com>
2022-01-07 22:24:13 +0700

    Add Code Khai bao 1 doi tuong, ham dung va phuong thuc goi doi tuong trong ham main

--- bin/practice_dart.dart
+++ bin/practice_dart.dart
@@ -1,5 +1,34 @@
-import 'package:practice_dart/practice_dart.dart' as practice_dart;
-
 void main(List<String> arguments) {
-  print('Hello world: ${practice_dart.calculate()}!');
+  print('Hello world!');
+
+  Student student = new Student("Trung", "Nam", 12, "TPHCM", 28.0, 10.0);
+
+  print("Tên: ${student._hoVaTen} - Giới tính: ${student._gioiTinh} - Tuổi: ${student._tuoi}");
+  // khai báo hàm tinhTong có trong class Student
+  print("${student.tinhTong()}");
+ 
+ 
 }
+
+
+
+// khai báo 1 đối tượng
+class Student {
+  String _hoVaTen;
+  String _gioiTinh;
+  int _tuoi;
+  String _queQuan;
+  double _diemVan;
+  double _diemToan;
+
+  // hàm khởi tạo
+  Student(this._hoVaTen, this._gioiTinh, this._tuoi, this._queQuan,
+      this._diemVan, this._diemToan);
+
+  // các phương thức
+  double tinhTong() {
+    return _diemToan + _diemVan;
+  }
+}
+
+
