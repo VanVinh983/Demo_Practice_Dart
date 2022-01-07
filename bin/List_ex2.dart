774900903734f448ef3d0a605a1c47618db84631
VanVinh983 <66901012+VanVinh983@users.noreply.github.com>
2022-01-07 22:32:51 +0700

    vanvinh learn List dart

---
+++ bin/List_ex2.dart
@@ -0,0 +1,11 @@
+import 'dart:collection';
+
+void main() {
+  // A collection chứa các phần tử không trùng lặp
+  var iterator = Set<String>.identity();
+  iterator.add('Tom');
+  iterator.add('Jerry');
+  iterator.add('Donald');
+  var list = List<String>.of(iterator, growable: true);
+  print(list);
+}
