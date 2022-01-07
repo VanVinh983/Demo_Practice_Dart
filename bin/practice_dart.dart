d09169e68588d0276f7ba37707817d0c981da7c0
EvolViper <74658373+EvolViper@users.noreply.github.com>
2022-01-07 22:29:31 +0700

    Add

--- bin/practice_dart.dart
+++ bin/practice_dart.dart
@@ -6,11 +6,29 @@ void main(List<String> arguments) {
   print("Tên: ${student._hoVaTen} - Giới tính: ${student._gioiTinh} - Tuổi: ${student._tuoi}");
   // khai báo hàm tinhTong có trong class Student
   print("${student.tinhTong()}");
- 
- 
+
+  // khai báo hàm variable
+  variable();
 }
 
+// hàm khai báo biến
+void variable() {
+  // khai báo biến
+  var a = "Tôi là người mới";
+  //a = 10; // do biến var khai báo giá trị đầu tiên theo kiêu String
+  dynamic b = "Thôi đi ngủ đây!"; // do từ khóa dynamic nên các giá trị của biến b có thể thay đổi 
+  b = 10;
+  int c = 10;
+  print("${a}");
+  print("${b}");
+  print("${c}");
+  
+  final List<int> list = [1, 2, 3, 4, 5];
+  list.add(6);
+  print(list.toList());
 
+  const List<int> list2 = [0, 1, 23, 4]; // giá trị của biến với từ khóa const là giá trị cố định. Không thể thay đổi giá trị của biến có từ khóa là const.
+}
 
 // khai báo 1 đối tượng
 class Student {
