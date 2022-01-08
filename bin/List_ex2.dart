b678f14af5e48d3cd3bbb19e3c897da3bdee8bea
VanVinh983 <66901012+VanVinh983@users.noreply.github.com>
2022-01-08 10:17:51 +0700

    vanvinh update file list_ex2

--- bin/List_ex2.dart
+++ bin/List_ex2.dart
@@ -7,5 +7,5 @@ void main() {
   iterator.add('Jerry');
   iterator.add('Donald');
   var list = List<String>.of(iterator, growable: true);
-  print(list);
+  print('danh sach phan tu la: $list');
 }
